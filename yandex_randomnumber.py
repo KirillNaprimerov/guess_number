c1c7345c1ad6cea972687f3590dc6806d625a64b
kirill.naprimerov <tvojd3292@gmail.com>
2025-03-20 17:25:57 +0300

    optimize key logic

--- yandex_randomnumber.py
+++ yandex_randomnumber.py
@@ -6,10 +6,10 @@ while True:
     user_number = int(input('Введите своё число от 1 до 100: '))
     if 100 < user_number < 0:
         print('Uncorrect number!')
-    if computer_number > user_number:
+    elif computer_number > user_number:
         print('Your number is less than exprcted :(')
-    if computer_number < user_number:
+    elif computer_number < user_number:
         print('Your number is more than expected :(')
-    if computer_number == user_number:
+    elif computer_number == user_number:
         print('Wonderful!')
         break
